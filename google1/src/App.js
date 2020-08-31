c14bb6c047389868869a4fb0cea9470328a83ebe
Mark Force <force.j.mark@gmail.com>
2020-08-31 15:21:18 -0400

    added search functionality

--- google1/src/App.js
+++ google1/src/App.js
@@ -10,20 +10,71 @@ class App extends React.Component{
     this.state = {
       books: [],
       error: null,
-      params:{
-        
-       }
-
+      loading: false,
+      q: '',
+      filter: '',
+      printType: '',
+      key: 'AIzaSyA9ET2UqWKLWV0Fh3MSXLtCeZtL3PerURI',
     }
   }
+
+  handleFilterPrintType = (e) => {
+    this.setState({
+      printType: e
+      })
+  }
+
+  handleSearchText = (e) => {
+    this.setState({
+      q: e
+      })
+  }
+
+  handleFilterBookType = (e) => {
+    this.setState({
+      filter: e
+      })
+  }
+
+  handleApiFetch = (url) => {
+    fetch(url) 
+      .then(res => {
+        if(res.ok) {
+          return res.json()
+        }
+        throw new Error(res.status)
+      })
+      .then(data => {
+          console.log(data)
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+    }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault()
+    let url = `https://www.googleapis.com/books/v1/volumes?filter=${this.state.filter}&printType=${this.state.printType}&q=${this.state.q}`
+    this.handleApiFetch(url)
+    console.log(url)
+  }
+
+
+
+//on form submit call handleApiFetch
+//return the results to SearchResults.js
+
+
+
+
+
   render(){
   return (
     <div className="App">
       <Header />
-      <Searchbox />
+      <Searchbox props={this.state} handleSubmit={this.handleFormSubmit} handleFilterByType={this.handleFilterBookType}  handleSearchText={this.handleSearchText} handleSearch={this.handleFilterPrintType}/>
     </div>
-
-  )
+   )
   }
 }
 
--- google1/src/components/Searchbox.js
+++ google1/src/components/Searchbox.js
@@ -4,28 +4,39 @@ export default class Searchbox extends React.Component{
         render(){ 
             return(
                 <div className='SearchBar'>
-                    <form >
+    
                     <label htmlFor='SearchInput'className='SearchLabel'>search:</label>
-                        <input name='SearchInput' id='SearchInput' placeholder= 'Book name'type='text'></input>
-                        <button type='submit' >Search</button>
+                        <input 
+                        name='SearchInput' 
+                        value={this.props.props.q} 
+                        id='SearchInput' 
+                        placeholder='Book name' 
+                        type='text'
+                        onChange={e => this.props.handleSearchText(e.target.value)}
+                        
+                        ></input>
+                        <button type='submit' onClick={e => this.props.handleSubmit(e)} >Search</button>
                         <br /> 
                         <label htmlFor='filterByType'>print type:</label>
-                        <select id='filterByType'name='filterByType'>
-                            <option value='all'>all</option>
-                            <option value='books'>books</option>
-                            <option value='magazines'>magazines</option>
-                            
+                        <select id='filterByType'
+                        name='filterByType'
+                        onChange={e => this.props.handleSearch(e.target.value)}>
+                            <option value='ALL'>all</option>
+                            <option value='BOOKS'>books</option>
+                            <option value='MAGAZINES'>magazines</option>
                         </select>
                         <label htmlFor='filter'>book type:</label>
-                        <select id='filter'name='filter'>
+                        <select id='filter'name='filter'
+                        onChange={e => this.props.handleFilterByType(e.target.value)}>
                         <option value='ebooks'>ebooks</option>
-                        <option value='free-EBooks'>free EBooks</option>
+                        <option value='free-ebooks'>free EBooks</option>
                         <option value='full'>full</option>
                         <option value='partial'>partial</option>
                         </select>
-                        </form>
+                    
                      </div>
             )
         }
 
 }
+
